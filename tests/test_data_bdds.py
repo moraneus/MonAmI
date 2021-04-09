1939c40e3d17aeb45389c4df50252d1ef477a604
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-09 21:43:00 +0300

    Update test_data_bdds.py

--- tests/test_data_bdds.py
+++ tests/test_data_bdds.py
@@ -3,7 +3,7 @@ import unittest
 
 
 class TestDataBdds(unittest.TestCase):
-    def test_data_1_all_data_is_the_same_interval_is_extended(self):
+    def test_data_1_all_data_is_the_same_interval_and_data_not_extended(self):
         sequence = [["begin", 'Boo', 'Data1'], ["end", 'Boo'], ["begin", 3, 'Data1'], ["end", 3]]
         results = {
             'XD': [{'D0': False}]
@@ -14,7 +14,21 @@ class TestDataBdds(unittest.TestCase):
         for key in results.keys():
             self.assertCountEqual(results[key], list(bdds[key]))
 
-    def test_data_2_all_data_is_the_same_interval_is_extended(self):
+    def test_data_2_all_data_is_the_same_interval_is_extended_data_is_not(self):
+        sequence = [["begin", 1, 'Data1'], ["end", 1], ["begin", 2, 'Data1'], ["end", 2],
+                    ["begin", 3, 'Data1'], ["end", 3]]
+        results = {
+            'XD': [{'X0': False, 'X1':False, 'X2': False, 'D0': False},
+                   {'X0': False, 'X1': False, 'X2': True, 'D0': False},
+                   {'X0': False, 'X1': True, 'X2': False, 'D0': False}]
+        }
+
+        bdds = update_bdds(sequence, i_num_of_variables=1)
+
+        for key in results.keys():
+            self.assertCountEqual(results[key], list(bdds[key]))
+
+    def test_data_3_all_data_is_the_same_interval_is_extended_data_is_not(self):
         sequence = [["begin", 'Boo', 'Data1'], ["end", 'Boo'], ["begin", 3, 'Data1'], ["end", 3],
                     ["begin", 2, "Data1"], ["end", 2], ["begin", 4, "Data1"], ["end", 4]]
         results = {
@@ -27,7 +41,7 @@ class TestDataBdds(unittest.TestCase):
             self.assertCountEqual(results[key], list(bdds[key]))
 
 
-    def test_data_3_all_data_is_not_the_same_interval_is_extended(self):
+    def test_data_4_all_data_is_not_the_same_interval_and_data_is_not_extended(self):
         sequence = [["begin", 1, 'Data1'], ["end", 1], ["begin", 2, 'Data2'], ["end", 2]]
         results = {
             'XD': [{'X0': False, 'D0': False},
@@ -39,21 +53,7 @@ class TestDataBdds(unittest.TestCase):
         for key in results.keys():
             self.assertCountEqual(results[key], list(bdds[key]))
 
-    def test_data_4_data_is_not_the_same_interval_and_data_is_extended(self):
-        sequence = [["begin", 1, 'Data1'], ["end", 1], ["begin", 2, 'Data2'], ["end", 2],
-                    ["begin", 3, "Data1"], ["end", 3]]
-        results = {
-            'XD': [{'X0': False, 'X1': False, 'X2': False, 'D0': False, 'D1': False, 'D2': False},
-                   {'X0': False, 'X1': False, 'X2': True,'D0': False, 'D1': False, 'D2': True},
-                   {'X0': False, 'X1': True, 'X2': False, 'D0': False, 'D1': False, 'D2': False}]
-        }
-
-        bdds = update_bdds(sequence, i_num_of_variables=1)
-
-        for key in results.keys():
-            self.assertCountEqual(results[key], list(bdds[key]))
-
-    def test_data_5_all_data_is_not_the_same_interval_and_data_is_extended(self):
+    def test_data_5_data_is_not_the_same_interval_and_data_is_extended(self):
         sequence = [["begin", 1, 'Data1'], ["end", 1], ["begin", 2, 'Data2'], ["end", 2],
                     ["begin", 3, "Data3"], ["end", 3]]
         results = {
@@ -67,7 +67,21 @@ class TestDataBdds(unittest.TestCase):
         for key in results.keys():
             self.assertCountEqual(results[key], list(bdds[key]))
 
-    def test_data_5_all_data_is_not_the_same_interval_and_data_is_extended(self):
+    def test_data_6_all_data_is_not_the_same_interval_and_data_is_extended(self):
+        sequence = [["begin", 1, 'Data1'], ["end", 1], ["begin", 2, 'Data2'], ["end", 2],
+                    ["begin", 3, "Data3"], ["end", 3]]
+        results = {
+            'XD': [{'X0': False, 'X1': False, 'X2': False, 'D0': False, 'D1': False, 'D2': False},
+                   {'X0': False, 'X1': False, 'X2': True,'D0': False, 'D1': False, 'D2': True},
+                   {'X0': False, 'X1': True, 'X2': False, 'D0': False, 'D1': True, 'D2': False}]
+        }
+
+        bdds = update_bdds(sequence, i_num_of_variables=1)
+
+        for key in results.keys():
+            self.assertCountEqual(results[key], list(bdds[key]))
+
+    def test_data_7_all_data_is_not_the_same_interval_and_data_is_extended(self):
         sequence = [["begin", 1, 'Data1'], ["end", 1], ["begin", 2, 'Data2'], ["end", 2],
                     ["begin", 3, "Data3"], ["end", 3], ["begin", 4, "Data4"], ["end", 4],
                     ["begin", 5, "Data5"], ["end", 5], ["begin", 6, "Data6"], ["end", 6],
@@ -100,9 +114,7 @@ class TestDataBdds(unittest.TestCase):
         bdds = update_bdds(sequence, i_num_of_variables=1)
 
         for key in results.keys():
-            print("#" * 50)
-            print(list(bdds[key]))
-            print("#" * 50)
             self.assertCountEqual(results[key], list(bdds[key]))
+
 
 
