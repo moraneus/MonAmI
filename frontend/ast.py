b2c053482c3b7d936398b1167e2764a124530d54
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-19 17:55:48 +0300

    Update ast.py

--- frontend/ast.py
+++ frontend/ast.py
@@ -5,6 +5,7 @@
 # Abstract syntax (AST nodes) for the MonAmi specification logic
 # --------------------------------------------------------------
 from typing import Set
+from graphics.io import IO
 
 
 def error(msg : str):
@@ -38,6 +39,15 @@ class And(Formula):
     def free_vars(self) -> Set[str]:
         return self.formula1.free_vars() | self.formula2.free_vars()
 
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = self.formula1.eval(bdd_manager, data_hashtable, debug) & \
+                 self.formula2.eval(bdd_manager, data_hashtable, debug)
+
+        if debug:
+            IO.subformula(f"({self.formula1}) & ({self.formula2}))", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
+
 
 class Or(Formula):
     def __init__(self, formula1, formula2):
@@ -52,6 +62,15 @@ class Or(Formula):
 
     def free_vars(self) -> Set[str]:
         return self.formula1.free_vars() | self.formula2.free_vars()
+
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = self.formula1.eval(bdd_manager, data_hashtable, debug) | \
+                 self.formula2.eval(bdd_manager, data_hashtable, debug)
+
+        if debug:
+            IO.subformula(f"({self.formula1}) | ({self.formula2}))", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
 
 
 class Implies(Formula):
@@ -68,6 +87,15 @@ class Implies(Formula):
     def free_vars(self) -> Set[str]:
         return self.formula1.free_vars() | self.formula2.free_vars()
 
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = ~self.formula1.eval(bdd_manager, data_hashtable, debug) | \
+                 self.formula2.eval(bdd_manager, data_hashtable, debug)
+
+        if debug:
+            IO.subformula(f"({self.formula1}) -> ({self.formula2}))", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
+
 
 class Not(Formula):
     def __init__(self, formula):
@@ -81,6 +109,15 @@ class Not(Formula):
 
     def free_vars(self) -> Set[str]:
         return self.formula.free_vars()
+
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = ~self.formula.eval(bdd_manager, data_hashtable)
+
+        if debug:
+            if debug:
+                IO.subformula(f"~({self.formula}))", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
 
 
 class Before(Formula):
@@ -97,6 +134,15 @@ class Before(Formula):
     def free_vars(self) -> Set[str]:
         return {self.interval1, self.interval2}
 
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = bdd_manager.rename('XXYY', self.interval1, self.interval2)
+
+        if debug:
+            if debug:
+                IO.subformula(f"{self.interval1} < {self.interval2}", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
+
 
 class Overlaps(Formula):
     def __init__(self, interval1, interval2):
@@ -111,6 +157,15 @@ class Overlaps(Formula):
 
     def free_vars(self) -> Set[str]:
         return {self.interval1, self.interval2}
+
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = bdd_manager.rename('XYXY', self.interval1, self.interval2)
+
+        if debug:
+            if debug:
+                IO.subformula(f"{self.interval1} o {self.interval2}", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
 
 
 class Includes(Formula):
@@ -127,11 +182,19 @@ class Includes(Formula):
     def free_vars(self) -> Set[str]:
         return {self.interval1, self.interval2}
 
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = bdd_manager.rename('XYYX', self.interval1, self.interval2)
+
+        if debug:
+            IO.subformula(f"{self.interval1} i {self.interval2}", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
+
 
 class Data(Formula):
     def __init__(self, interval, data):
         self.interval = interval
-        self.data = data
+        self.data = data[1:-1]
 
     def __str__(self):
         return f'{self.interval}({self.data})'
@@ -141,6 +204,18 @@ class Data(Formula):
 
     def free_vars(self) -> Set[str]:
         return {self.interval}
+
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        data_bitstring = data_hashtable.lookup_no_update(self.data)
+        if not(data_bitstring):
+            result = bdd_manager.bdd_manager.false
+        else:
+            result = bdd_manager.restrict(data_bitstring, self.interval)
+
+        if debug:
+            IO.subformula(f"{self.interval}({self.data})", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
 
 
 class Same(Formula):
@@ -157,6 +232,17 @@ class Same(Formula):
     def free_vars(self) -> Set[str]:
         return {self.interval1, self.interval2}
 
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        bdd1 = bdd_manager.rename('XD', self.interval1)
+        bdd2 = bdd_manager.rename('XD', self.interval2)
+
+        result = bdd_manager.exist(['D'], bdd1 & bdd2)
+
+        if debug:
+            IO.subformula(f"same({self.interval1}, {self.interval2})", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
+
 
 class Exist(Formula):
     def __init__(self, intervals, formula):
@@ -172,6 +258,15 @@ class Exist(Formula):
     def free_vars(self) -> Set[str]:
         return self.formula.free_vars() - set(self.intervals)
 
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = bdd_manager.exist(self.intervals, self.formula.eval(bdd_manager, data_hashtable, debug))
+
+        if debug:
+            IO.subformula(f"exist {self.intervals} . {repr(self.formula)}", list(bdd_manager.bdd_manager.pick_iter(result)))
+
+        return result
+
+
 
 class Paren(Formula):
     def __init__(self, formula):
@@ -185,3 +280,11 @@ class Paren(Formula):
 
     def free_vars(self) -> Set[str]:
         return self.formula.free_vars()
+
+    def eval(self, bdd_manager, data_hashtable, debug=True):
+        result = self.formula.eval(bdd_manager, data_hashtable, debug)
+
+        if debug:
+            IO.subformula(f"PAREN {repr(self.formula)}")
+
+        return result
