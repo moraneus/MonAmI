c2371329a972e74d4e532ac2e0e943ef37dab872
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-19 18:07:33 +0300

    Update test_helper.py

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,12 +1,12 @@
 from logic.bdd_atl import BddAtl
 from logic.bitstring_table import BitstringTable
-from execptions.interval_execptions import IntervalDataError
+from execptions.execptions import IntervalDataError
 
 
-def update_bdds(i_sequence_of_events, i_num_of_variables):
-    a = BddAtl(i_num_of_variables=i_num_of_variables, i_debug=False)
-    interval_hash_table = BitstringTable(i_bit_string_length=i_num_of_variables)
-    data_hash_table = BitstringTable(i_bit_string_length=i_num_of_variables)
+def update_bdds_without_specification(i_sequence_of_events, i_num_of_variables, i_expansion_length):
+    bdd_atl = BddAtl([], i_interval_size=i_num_of_variables, i_debug=False)
+    interval_hash_table = BitstringTable(i_num_of_variables, i_expansion_length)
+    data_hash_table = BitstringTable(i_num_of_variables, i_expansion_length)
 
     for event in i_sequence_of_events:
         event_type = event[0]
@@ -19,8 +19,36 @@ def update_bdds(i_sequence_of_events, i_num_of_variables):
                 raise IntervalDataError(interval_id)
 
             data_bitstring = data_hash_table.lookup(event_type, data)
-            a.event_update(event_type, interval_id, interval_bitstring, data_bitstring)
+            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data_bitstring)
         else:
-            a.event_update(event_type, interval_id, interval_bitstring)
+            bdd_atl.event_update(event_type, interval_id, interval_bitstring)
 
-    return a.bdds
+    return bdd_atl.bdds
+
+
+def update_bdds_with_specification(i_sequence_of_events, i_specification, i_num_of_variables):
+    bdd_atl = BddAtl(i_specification.intervals, i_interval_size=i_num_of_variables, i_debug=False)
+    interval_hash_table = BitstringTable(i_num_of_variables)
+    data_hash_table = BitstringTable(i_num_of_variables)
+
+    for event in i_sequence_of_events:
+        event_type = event[0]
+        interval_id = event[1]
+        interval_bitstring = interval_hash_table.lookup(event_type, interval_id)
+        if event_type == "begin":
+            try:
+                data = event[2]
+            except IndexError:
+                raise IntervalDataError(interval_id)
+
+            data_bitstring = data_hash_table.lookup(event_type, data)
+            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data_bitstring)
+        else:
+            bdd_atl.event_update(event_type, interval_id, interval_bitstring)
+
+        result = i_specification.eval(bdd_atl, data_hash_table) == bdd_atl.bdd_manager.true
+
+        if result:
+            break
+
+    return result
