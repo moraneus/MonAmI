390b3f18bf880878f1c0e430f07a532fcdf4c4e0
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-19 18:04:42 +0300

    Update bitstring_table.py

--- logic/bitstring_table.py
+++ logic/bitstring_table.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 class BitstringTable:
-    def __init__(self, i_bit_string_length=3, i_number_of_growth=2):
+    def __init__(self, i_bitstring_length=3, i_expansion_length=2):
         self.__m_bitstring_table = {}
-        self._m_bit_string_length = i_bit_string_length
-        self.__m_number_of_growth = i_number_of_growth
-        self.__max_items_allowed = 2**i_bit_string_length - 1
+        self._m_bitstring_length = i_bitstring_length
+        self.__m_number_of_growth = i_expansion_length
+        self.__max_items_allowed = 2 ** i_bitstring_length
 
     def lookup(self, i_event_type: str, i_key)->str:
         """
@@ -15,6 +15,7 @@ class BitstringTable:
 
         Parameters
         ----------
+        i_event_type : The type of the event ("begin", "end" or "data").
         i_key : The key variable before it converted into bitstring.
 
         Returns
@@ -26,14 +27,34 @@ class BitstringTable:
         # Update occur when it reached to bitstring length limit.
         # The new size is the old size with 2 more bits
         if i_event_type == 'begin' and len(self.__m_bitstring_table.keys()) >= self.__max_items_allowed:
-            self.__length_update(self._m_bit_string_length + self.__m_number_of_growth)
+            self.__length_update(self._m_bitstring_length + self.__m_number_of_growth)
 
         # Set the value of the bitstring to be equal to the number of new keys have been watched until now
         # The counting start from 0.
         if i_key not in self.__m_bitstring_table.keys():
             self.__m_bitstring_table[i_key] = np.binary_repr(len(self.__m_bitstring_table.keys()),
-                                                             width=self._m_bit_string_length)
+                                                             width=self._m_bitstring_length)
         return self.__m_bitstring_table[i_key]
+
+    def lookup_no_update(self, i_key):
+        """
+        Lookup on the bitstring table without updating.
+        Just query the dictionary.
+
+        Parameters
+        ----------
+        i_key : The key variable before it converted into bitstring.
+
+        Returns
+        -------
+        The representation of the key in bitstring if key exist, other wise False.
+
+        """
+
+        if i_key not in self.__m_bitstring_table.keys():
+            return False
+        return self.__m_bitstring_table[i_key]
+
 
     def __length_update(self, i_new_length: int):
         """
@@ -50,8 +71,8 @@ class BitstringTable:
         """
 
         # Create 0's prefix padding in a szie of the defined growth.
-        prefix = '0' * (i_new_length - self._m_bit_string_length)
-        self._m_bit_string_length = i_new_length
+        prefix = '0' * (i_new_length - self._m_bitstring_length)
+        self._m_bitstring_length = i_new_length
 
         # Update all values seen with the prefix.
         for key, bitstring in self.__m_bitstring_table.items():
@@ -67,4 +88,4 @@ class BitstringTable:
         None
 
         """
-        self.__max_items_allowed = self._m_bit_string_length**2 - 1
+        self.__max_items_allowed = self._m_bitstring_length ** 2 - 1
