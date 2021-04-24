15908a8d8aa482d359208e32e2c4ef7402b893e7
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-24 10:36:56 +0300

    Update X, Y, D variables into _X, _Y, _D variables.

--- tests/test_before_relation.py
+++ tests/test_before_relation.py
@@ -6,7 +6,7 @@ class TestBeforeRelation(unittest.TestCase):
     def test_before_1_pass(self):
         sequence = [["begin", 'Boo', 'Data1'], ["end", 'Boo'], ["begin", 3, 'Data2'], ["end", 3]]
         results = {
-            'XXYY': [{'X0': False, 'Y0': True}],
+            'XXYY': [{'_X0': False, '_Y0': True}],
             'XYXY': [],
             'XYYX': []
         }
@@ -20,9 +20,9 @@ class TestBeforeRelation(unittest.TestCase):
         sequence =  [["begin", 7, "Data1"], ["end", 7], ["begin", 2, "Data1"], ["end", 2],
                      ["begin", 3, "Data2"], ["end", 3]]
         results = {
-            'XXYY': [{'X0': False, 'X1': False, 'X2': False, 'Y0': False, 'Y1': False, 'Y2': True},
-                     {'X0': False, 'X1': False, 'X2': False, 'Y0': False, 'Y1': True, 'Y2': False},
-                     {'X0': False, 'X1': False, 'X2': True, 'Y0': False, 'Y1': True, 'Y2': False}],
+            'XXYY': [{'_X0': False, '_X1': False, '_X2': False, '_Y0': False, '_Y1': False, '_Y2': True},
+                     {'_X0': False, '_X1': False, '_X2': False, '_Y0': False, '_Y1': True, '_Y2': False},
+                     {'_X0': False, '_X1': False, '_X2': True, '_Y0': False, '_Y1': True, '_Y2': False}],
             'XYXY': [],
             'XYYX': []
         }
@@ -35,12 +35,12 @@ class TestBeforeRelation(unittest.TestCase):
         sequence =  [["begin", 7, "Data1"], ["end", 7], ["begin", 2, "Data1"], ["end", 2],
                      ["begin", 3, "Data2"], ["end", 3], ["begin", 1, "Data1"], ["end", 1]]
         results = {
-            'XXYY': [{'X0': False, 'X1': False, 'X2': False, 'Y0': False, 'Y1': False, 'Y2': True},
-                     {'X0': False, 'X1': False, 'X2': False, 'Y0': False, 'Y1': True, 'Y2': False},
-                     {'X0': False, 'X1': False, 'X2': True, 'Y0': False, 'Y1': True, 'Y2': False},
-                     {'X0': False, 'X1': False, 'X2': False, 'Y0': False, 'Y1': True, 'Y2': True},
-                     {'X0': False, 'X1': False, 'X2': True, 'Y0': False, 'Y1': True, 'Y2': True},
-                     {'X0': False, 'X1': True, 'X2': False, 'Y0': False, 'Y1': True, 'Y2': True}],
+            'XXYY': [{'_X0': False, '_X1': False, '_X2': False, '_Y0': False, '_Y1': False, '_Y2': True},
+                     {'_X0': False, '_X1': False, '_X2': False, '_Y0': False, '_Y1': True, '_Y2': False},
+                     {'_X0': False, '_X1': False, '_X2': True, '_Y0': False, '_Y1': True, '_Y2': False},
+                     {'_X0': False, '_X1': False, '_X2': False, '_Y0': False, '_Y1': True, '_Y2': True},
+                     {'_X0': False, '_X1': False, '_X2': True, '_Y0': False, '_Y1': True, '_Y2': True},
+                     {'_X0': False, '_X1': True, '_X2': False, '_Y0': False, '_Y1': True, '_Y2': True}],
             'XYXY': [],
             'XYYX': []
         }
