518125fe8a95961877441a47b904ee36b23abb43
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-24 10:40:16 +0300

    Update the function to support the program changes.

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -5,8 +5,8 @@ from execptions.execptions import IntervalDataError
 
 def update_bdds_without_specification(i_sequence_of_events, i_num_of_variables, i_expansion_length):
     bdd_atl = BddAtl([], i_interval_size=i_num_of_variables, i_debug=False)
-    interval_hash_table = BitstringTable(i_num_of_variables, i_expansion_length)
-    data_hash_table = BitstringTable(i_num_of_variables, i_expansion_length)
+    interval_hash_table = BitstringTable(i_num_of_variables, i_expansion_length, i_debug=False)
+    data_hash_table = BitstringTable(i_num_of_variables, i_expansion_length, i_debug=False)
 
     for event in i_sequence_of_events:
         event_type = event[0]
@@ -19,7 +19,7 @@ def update_bdds_without_specification(i_sequence_of_events, i_num_of_variables, 
                 raise IntervalDataError(interval_id)
 
             data_bitstring = data_hash_table.lookup(event_type, data)
-            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data_bitstring)
+            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data, data_bitstring)
         else:
             bdd_atl.event_update(event_type, interval_id, interval_bitstring)
 
@@ -28,8 +28,8 @@ def update_bdds_without_specification(i_sequence_of_events, i_num_of_variables, 
 
 def update_bdds_with_specification(i_sequence_of_events, i_specification, i_num_of_variables):
     bdd_atl = BddAtl(i_specification.intervals, i_interval_size=i_num_of_variables, i_debug=False)
-    interval_hash_table = BitstringTable(i_num_of_variables)
-    data_hash_table = BitstringTable(i_num_of_variables)
+    interval_hash_table = BitstringTable(i_num_of_variables, i_debug=False)
+    data_hash_table = BitstringTable(i_num_of_variables, i_debug=False)
 
     for event in i_sequence_of_events:
         event_type = event[0]
@@ -42,11 +42,13 @@ def update_bdds_with_specification(i_sequence_of_events, i_specification, i_num_
                 raise IntervalDataError(interval_id)
 
             data_bitstring = data_hash_table.lookup(event_type, data)
-            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data_bitstring)
+            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data, data_bitstring)
         else:
             bdd_atl.event_update(event_type, interval_id, interval_bitstring)
 
-        result = i_specification.eval(bdd_atl, data_hash_table) == bdd_atl.bdd_manager.true
+        result = i_specification.eval(bdd_manager=bdd_atl,
+                                      data_manager=data_hash_table,
+                                      debug_mode=True) == bdd_atl.bdd_manager.true
 
         if result:
             break
