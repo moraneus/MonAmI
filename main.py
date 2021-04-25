7a47135c3b59583be6da5fa9c89c89c4ce3c1c98
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-25 22:51:52 +0300

    Update main.py for supporting BDD['XD'] update at 'end' event.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from logic.bitstring_table import BitstringTable
 from logic.bdd_atl import BddAtl
 from graphics.io import IO
 from os import sep
-from execptions.execptions import IntervalDataError
+from execptions.execptions import IntervalDataError, BadEventValueError, EndsBeforeBeginError
 from frontend.parser import parse
 
 
@@ -15,11 +15,13 @@ def read_json(i_json_file):
 
 def main():
     IO.banner()
+
+    # Read input files
     configuration = read_json(f"input{sep}configuration")
     execution = read_json(f"input{sep}trace")["execution"]
     property = parse(read_json(f"input{sep}property")["property"].replace("'", '"'))
 
-
+    # Print input to console
     if configuration["DEBUG"]:
         IO.seperator("START")
         IO.execution(execution)
@@ -29,54 +31,65 @@ def main():
     bdd_atl = BddAtl(property.intervals,
                      i_interval_size=configuration["INTERVAL_SIZE"],
                      i_debug=configuration["DEBUG"])
+
     # Interval bitstring DB object
     interval_hash_table = BitstringTable(configuration["INTERVAL_SIZE"],
                                          configuration["EXPANSION_LENGTH"],
                                          i_debug=configuration["DEBUG"])
+
     # Data bitstring DB object
     data_hash_table = BitstringTable(configuration["DATA_SIZE"],
                                      configuration["EXPANSION_LENGTH"],
                                      i_debug=configuration["DEBUG"])
 
+    # Define an empty dictionary which map interval into data
+    interval_data_dict = {}
+
     for event in execution:
         try:
             event_type = event[0]
-            interval = event[1]
-            interval_bitstring = interval_hash_table.lookup(event_type, interval)
+            interval_id = event[1]
+            interval_bitstring = interval_hash_table.lookup(event_type, interval_id)
 
+            # Add the interval: data into the interval_data_dict
             if event_type == "begin":
                 # In a case when "begin" event doesn't contain a data.
                 try:
                     data = event[2]
-                except IndexError:
-                    raise IntervalDataError(interval)
+                    data_bitstring = data_hash_table.lookup(event_type, data)
+                    interval_data_dict[interval_id] = {"data": data, "data_bitstring": data_bitstring}
 
-                data_bitstring = data_hash_table.lookup(event_type, data)
-                bdd_atl.event_update(event_type, interval, interval_bitstring, data, data_bitstring)
-            else:
-                bdd_atl.event_update(event_type, interval, interval_bitstring)
+                except IndexError:
+                    raise IntervalDataError(interval_id)
+
+            # because data at the 'end' event is not mandatory, several exceptions are catches here
+            if interval_id not in interval_data_dict.keys():
+                if event_type == 'end':
+                    raise EndsBeforeBeginError(interval_id)
+                else:
+                    raise BadEventValueError(interval_id)
+
+            # Call the main BDD update function
+            bdd_atl.event_update(event_type, interval_id, interval_bitstring, interval_data_dict[interval_id])
 
             if configuration["DEBUG"]:
                 IO.ast_header()
 
+            # Check if the property is satisfied
             result = property.eval(bdd_manager = bdd_atl,
                                    data_manager = data_hash_table,
                                    debug_mode = configuration["DEBUG"]) == bdd_atl.bdd_manager.true
 
+            # Print the result for the current event
             if configuration["DEBUG"]:
                 if result:
                     IO.true()
                 else:
                     IO.false()
 
+            # Print the final state of the BDDs when the property is satisfied.
             if result:
-                IO.seperator('FINAL STATE')
-                IO.execution(execution)
-                IO.property(property)
-                for bdd_name, bdd_data in bdd_atl.bdds.items():
-                    IO.bdd_state(bdd_name, bdd_data)
-                IO.true()
-                IO.seperator('THE END')
+                IO.final(execution, property, bdd_atl.bdds.items())
                 break
 
         except Exception as err:
