5cf6e3e414c20587c113542fc6bb0a2f03841075
moraneus <48603901+moraneus@users.noreply.github.com>
2021-04-25 22:48:23 +0300

    Update test_helper.py for supporting update BDD['XD'] at 'end' event.

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,27 +1,35 @@
 from logic.bdd_atl import BddAtl
 from logic.bitstring_table import BitstringTable
-from execptions.execptions import IntervalDataError
+from execptions.execptions import IntervalDataError, EndsBeforeBeginError, BadEventValueError
 
 
 def update_bdds_without_specification(i_sequence_of_events, i_num_of_variables, i_expansion_length):
     bdd_atl = BddAtl([], i_interval_size=i_num_of_variables, i_debug=False)
     interval_hash_table = BitstringTable(i_num_of_variables, i_expansion_length, i_debug=False)
     data_hash_table = BitstringTable(i_num_of_variables, i_expansion_length, i_debug=False)
+    interval_data_dict = {}
 
     for event in i_sequence_of_events:
         event_type = event[0]
         interval_id = event[1]
         interval_bitstring = interval_hash_table.lookup(event_type, interval_id)
+
+        # Save the data which relate to an interval in a dict
         if event_type == "begin":
             try:
                 data = event[2]
+                data_bitstring = data_hash_table.lookup(event_type, data)
+                interval_data_dict[interval_id] = {"data": data, "data_bitstring": data_bitstring}
             except IndexError:
                 raise IntervalDataError(interval_id)
 
-            data_bitstring = data_hash_table.lookup(event_type, data)
-            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data, data_bitstring)
-        else:
-            bdd_atl.event_update(event_type, interval_id, interval_bitstring)
+        if interval_id not in interval_data_dict.keys():
+            if event_type == 'end':
+                raise EndsBeforeBeginError(interval_id)
+            else:
+                raise BadEventValueError(interval_id)
+
+        bdd_atl.event_update(event_type, interval_id, interval_bitstring, interval_data_dict[interval_id])
 
     return bdd_atl.bdds
 
@@ -30,21 +38,23 @@ def update_bdds_with_specification(i_sequence_of_events, i_specification, i_num_
     bdd_atl = BddAtl(i_specification.intervals, i_interval_size=i_num_of_variables, i_debug=False)
     interval_hash_table = BitstringTable(i_num_of_variables, i_debug=False)
     data_hash_table = BitstringTable(i_num_of_variables, i_debug=False)
+    interval_data_dict = {}
 
     for event in i_sequence_of_events:
         event_type = event[0]
         interval_id = event[1]
         interval_bitstring = interval_hash_table.lookup(event_type, interval_id)
+
+        # Save the data which relate to an interval in a dict
         if event_type == "begin":
             try:
                 data = event[2]
+                data_bitstring = data_hash_table.lookup(event_type, data)
+                interval_data_dict[interval_id] = {"data": data, "data_bitstring": data_bitstring}
             except IndexError:
                 raise IntervalDataError(interval_id)
 
-            data_bitstring = data_hash_table.lookup(event_type, data)
-            bdd_atl.event_update(event_type, interval_id, interval_bitstring, data, data_bitstring)
-        else:
-            bdd_atl.event_update(event_type, interval_id, interval_bitstring)
+        bdd_atl.event_update(event_type, interval_id, interval_bitstring, interval_data_dict[interval_id])
 
         result = i_specification.eval(bdd_manager=bdd_atl,
                                       data_manager=data_hash_table,
