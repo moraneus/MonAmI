df03b6bac53fe4f1c4a8a879b1487418c61e1631
moraneus <moraneus@gmail.com>
2021-06-02 21:12:29 +0300

    Add modes support.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from logic.bitstring_table import BitstringTable
 from logic.bdd_atl import BddAtl
 from graphics.io import IO
 from os import sep
+import math
 from execptions.execptions import IntervalDataError
 from frontend.parser import parse
 
@@ -27,26 +28,36 @@ def main():
         IO.execution(execution)
         IO.property(property)
 
+    # Set the initial size of interval bitstring
+    if configuration['INTERVAL_SIZE'] == "AUTO":
+        interval_bitstring_length = math.ceil(math.log(len(execution) / 2, 2))
+    else:
+        interval_bitstring_length = configuration["INTERVAL_SIZE"]
+
+    # Set the initial size of data bitstring
+    if configuration['DATA_SIZE'] == "AUTO":
+        data_bitstring_length = math.ceil(math.log(len(execution) / 2, 2))
+    else:
+        data_bitstring_length = configuration["DATA_SIZE"]
+
     # BDD constructor object
     bdd_atl = BddAtl(property.get_intervals(),
-                     i_interval_size=configuration["INTERVAL_SIZE"],
+                     i_interval_size=interval_bitstring_length,
+                     i_data_size=data_bitstring_length,
                      i_debug=configuration["DEBUG"])
 
     # Interval bitstring DB object
-    interval_hash_table = BitstringTable(configuration["INTERVAL_SIZE"],
+    interval_hash_table = BitstringTable(interval_bitstring_length,
                                          configuration["EXPANSION_LENGTH"],
                                          i_debug=configuration["DEBUG"])
 
     # Data bitstring DB object
-    data_hash_table = BitstringTable(configuration["DATA_SIZE"],
+    data_hash_table = BitstringTable(data_bitstring_length,
                                      configuration["EXPANSION_LENGTH"],
                                      i_debug=configuration["DEBUG"])
 
     # Define an empty dictionary which map interval into data
     interval_data_dict = {}
-
-    # Define the verdicts result list
-    verdicts = []
 
     for event in execution:
         try:
@@ -76,42 +87,50 @@ def main():
             if configuration["DEBUG"]:
                 IO.ast_header()
 
-            # Check if the property is satisfied
-            result = property.eval(bdd_manager=bdd_atl,
-                                   data_manager=data_hash_table,
-                                   debug_mode=configuration["DEBUG"]) == bdd_atl.bdd_manager.true
+            # Check if the property is satisfied in the MODE ["VIOLATION", "SATISFACTION" or "CONTINUE-SMALL"]
+            if configuration["MODE"] != "CONTINUE-BIG":
+                result = property.eval(bdd_manager=bdd_atl,
+                                       data_manager=data_hash_table,
+                                       debug_mode=configuration["DEBUG"]) == bdd_atl.bdd_manager.true
 
-            # Print the result for the current event
-            if configuration["DEBUG"]:
-                if result:
-                    IO.true()
-                else:
-                    IO.false()
+                # Print the result for the current event
+                if configuration["DEBUG"]:
+                    if result:
+                        IO.true()
+                    else:
+                        IO.false()
 
-            verdicts.append(result)
-
-            # According to the MODE the tool will continue running or stopped.
-            # In a case of MODE == VIOLATION the tool stop when property eval results is False.
-            # In a case of MODE == SATISFACTION the tool stop when property eval results is True.
-            # Any other case the tool continue running until the end of the trace.
-            if configuration["MODE"] == "VIOLATION":
-                if not result:
-                    break
-            elif configuration["MODE"] == "SATISFACTION":
-                if result:
-                    break
-
+                # In a case of MODE == VIOLATION the tool stop when property eval results is False.
+                # In a case of MODE == SATISFACTION the tool stop when property eval results is True.
+                # In a case of MODE == CONTINUE-SMALL the tool evaluate the property after every event until the
+                # end of the trace.
+                # In a case of MODE == CONTINUE-BIG the tool evaluate the property once at the end of the trace.
+                # Any other case the tool will act the same as CONTINUE-BIG.
+                if configuration["MODE"] == "VIOLATION":
+                    if not result:
+                        break
+                elif configuration["MODE"] == "SATISFACTION":
+                    if result:
+                        break
 
         except Exception as err:
             IO.error(err)
             break
 
+    # Check if the property is satisfied in the MODE "CONTINUE-BIG"
+    if configuration["MODE"] not in ["VIOLATION", "SATISFACTION", "CONTINUE-SMALL"]:
+        result = property.eval(bdd_manager=bdd_atl,
+                               data_manager=data_hash_table,
+                               debug_mode=configuration["DEBUG"]) == bdd_atl.bdd_manager.true
+
     # Print the final BDDs state
     if configuration["DEBUG"]:
         IO.final(execution, property, bdd_atl.bdds.items())
 
-    # Print the verdicts at the end.
-    IO.verdicts(verdicts)
+    if result:
+        IO.true()
+    else:
+        IO.false()
 
 
 if __name__ == '__main__':
